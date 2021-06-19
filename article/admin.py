44ea01aace5e7c554004373e32cbe9b627ac43bd
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-06-19 23:39:36 +0300

    model Subject and model Article

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Subject, Article
 
-# Register your models here.
+admin.site.register(Subject)
+admin.site.register(Article)
--- article/models.py
+++ article/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import TextField
 
-# Create your models here.
+
+class Subject(models.Model):
+    class Meta:
+        db_table = "Тематика" #название таблицы в БД
+
+    subject_name = models.CharField(max_length=100) #название тематики
+    subject_description = TextField() # описание тематики
+
+    def __str__(self):
+        return self.subject_name
+
+
+class Article(models.Model):
+    class Meta:
+        db_table = "Статьи" #название таблицы в БД
+
+    article_user = models.ForeignKey(User, on_delete=models.CASCADE) # имя пользователя
+    article_subject = models.ForeignKey(Subject, on_delete=models.CASCADE) # привязка к тематике
+    article_name = models.CharField(max_length=100) # имя стати
+    article_content = TextField() # текст статьи
+    article_date = models.DateTimeField('Время создания статьи')
+
+    def __str__(self):
+        return self.article_name
