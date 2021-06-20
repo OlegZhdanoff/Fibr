64ff3968fd6a7a1fc3eb245f3745e949d16d1393
TRiG <gerysh07@gmail.com>
2021-06-20 16:41:50 +0300

    add static

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+console.log('123')
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.getElementById('navbar_top').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      }
+  });
+});
