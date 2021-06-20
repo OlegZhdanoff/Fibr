237aa5bd68f1d36931de75c21c5a0c2410f79cc8
Igor <gormal98@gmail.com>
2021-06-20 17:40:51 +0300

    add_models

--- article/models.py
+++ article/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=64)
+    short_description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = 'Тематика'
+        verbose_name_plural = 'Тематики'
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64, unique=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='article_images', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
