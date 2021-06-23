785494d0616bb907854b0bb11b996a82a46747ac
Kirill Ivanov <kirusha1991@gmail.com>
2021-06-24 00:34:43 +0500

    Поменял работу с шаблонами. Изменил базовый шаблон для хабов, для корректного отображения страниц. Меню теперь работает как вложенный шаблон.

--- hub/urls.py
+++ hub/urls.py
@@ -5,10 +5,6 @@ import hub.views as hub
 app_name = 'hub'
 
 urlpatterns = [
-    path('web-dev/', hub.WebDevView.as_view(), name='web-dev'),
-    path('administration/', hub.AdministrationView.as_view(), name='administration'),
-    path('design/', hub.DesignView.as_view(), name='design'),
-    path('management/', hub.ManagementView.as_view(), name='management'),
-    path('marketing/', hub.MarketingView.as_view(), name='marketing'),
+    path('<int:pk>/', hub.TopicView.as_view(), name='topic')
 
 ]
