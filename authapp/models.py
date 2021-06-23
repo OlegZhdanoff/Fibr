393159b1990ed8da4da9d85073315971c8f11425
Kirill Ivanov <kirusha1991@gmail.com>
2021-06-23 21:21:34 +0500

    Подготавливаюсь к рефакторингу вьюх под МВП.
    Добавил скрипт для заполнения базы (нужно доработать динамическую подстановку внешних ключей для статей)
    
    Изменил settings.py для исправления ошибки при работе с консолью

--- authapp/models.py
+++ authapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_avatars', blank=True)
     birthday = models.DateField(blank=True, null=True)
