6936a03885b4abfff19afecceff54dbcdabc79ca
Kirill Ivanov <kirusha1991@gmail.com>
2021-06-24 01:11:30 +0500

    Закончил работу с контроллерами

--- hub/views.py
+++ hub/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView
 
 from hub.models import Topic
+from article.models import Article
 
 
 class TopicView(DetailView):
@@ -9,4 +10,6 @@ class TopicView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.filter(topic=self.kwargs.get('pk'))
+
         return context
