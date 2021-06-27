e91d02acd8fd7024d081f284e7a6b93afab716f5
Igor <gormal98@gmail.com>
2021-06-27 15:59:36 +0300

    register view + form

---
+++ authapp/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from authapp.models import User
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
+        self.fields['email'].widget.attrs['placeholder'] = 'Введите адрес эл. почты'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Введите имя'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Введите фамилию'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Введите пароль'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Подтвердите пароль'
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
--- authapp/urls.py
+++ authapp/urls.py
@@ -7,5 +7,6 @@ app_name = 'authapp'
 urlpatterns = [
     path('login/', authapp.UserLogin.as_view(), name='login'),
     path('logout/', authapp.UserLogout.as_view(), name='logout'),
+    path('register/', authapp.RegisterUserView.as_view(), name='register'),
 
 ]
