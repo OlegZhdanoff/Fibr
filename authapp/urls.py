dfcab991fb2331b4125916b2e03cac416def4806
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-06-28 21:57:31 +0300

    user profile (models, views, forms)

--- authapp/urls.py
+++ authapp/urls.py
@@ -7,5 +7,5 @@ app_name = 'authapp'
 urlpatterns = [
     path('login/', authapp.UserLogin.as_view(), name='login'),
     path('logout/', authapp.UserLogout.as_view(), name='logout'),
-
+    path('profile/', authapp.profile, name='profile'),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+import article.models
+from authapp.forms import UserProfileForm
 
 
 class UserLogin(LoginView):
@@ -11,3 +16,18 @@ class UserLogout(LogoutView):
 
 def user_register():
     pass
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('authapp:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'form': form,
+        'article': article.models.Article,
+    }
+    return render(request, 'authapp/profile.html', context)
