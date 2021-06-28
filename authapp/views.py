fdd81d18e943d0ce8f6a962bffc6f4bc82b80da5
TRiG <gerysh07@gmail.com>
2021-06-28 22:48:26 +0300

    fix

--- authapp/views.py
+++ authapp/views.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
 from django.views.generic import CreateView
 
-from authapp.forms import UserRegisterForm, UserAuthenticationForm, UserProfileForm, UserProfileEditForm
 from authapp.models import User
+from authapp.forms import UserRegisterForm,UserAuthenticationForm
 
 
 class UserLogin(LoginView):
@@ -23,18 +20,3 @@ class RegisterUserView(CreateView):
     form_class = UserRegisterForm
     success_url = '/'
     success_msg = 'Пользователь успешно создан'
-
-
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileEditForm(data=request.POST, files=request.FILES, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('authapp:profile'))
-    else:
-        form = UserProfileForm(instance=request.user)
-
-    context = {
-        'form': form,
-    }
-    return render(request, 'authapp/profile.html', context)
