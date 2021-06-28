eb7dbe40e606173d591ff7221cc3b8fd95aa30cd
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-06-28 21:57:42 +0300

    user profile (models, views, forms)

---
+++ authapp/forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.forms import UserChangeForm
+from django import forms
+
+from authapp.models import User, UserProfile
+
+
+class UserProfileForm(UserChangeForm):
+    avatar = forms.ImageField(widget=forms.ClearableFileInput())
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'avatar', 'username', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['username'].widget.attrs['readonly'] = True
+        self.fields['email'].widget.attrs['readonly'] = True
+        self.fields['avatar'].widget.attrs['class'] = 'custom-file-input'
+
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('article', 'about_me', 'gender')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
