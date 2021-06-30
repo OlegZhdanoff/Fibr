799838631963cf456dab846c1e874c4724f4d063
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-06-30 22:47:45 +0300

    user profile

--- authapp/models.py
+++ authapp/models.py
@@ -2,13 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-
-
-# from article.models import ArticleView
+from article.models import *
 
 
 class User(AbstractUser):
-    image = models.ImageField(upload_to='users_avatars', blank=True)
+    image = models.ImageField(upload_to='users_avatars', blank=True, default='static/img/default.jpg')
     birthday = models.DateField(blank=True, null=True)
 
     def __str__(self):
@@ -27,16 +25,20 @@ class UserProfile(models.Model):
         (FEMALE, 'Ж'),
     )
     user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
-    # article = models.OneToOneField(ArticleView, blank=True)
-    article = models.TextField(blank=True, max_length=512, verbose_name='обо мне')
+    article = models.TextField(blank=True, max_length=512, verbose_name='статья')
     about_me = models.TextField(blank=True, max_length=512, verbose_name='обо мне')
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, verbose_name='пол')
 
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+
     @receiver(post_save, sender=User)
-    def create_user_profile(self, instance, created, **kwargs):
+    def create_user_profile(sender, instance, created, **kwargs):
         if created:
             UserProfile.objects.create(user=instance)
 
     @receiver(post_save, sender=User)
-    def save_user_profile(self, instance, **kwargs):
+    def save_user_profile(sender, instance, **kwargs):
         instance.userprofile.save()
