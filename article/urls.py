84b3f1fe37fcbe15c89944c3bc46c311fc026982
OlegZhdanov <stalker08@gmail.com>
2021-06-30 18:35:58 +0500

    edit article

--- article/urls.py
+++ article/urls.py
@@ -6,6 +6,6 @@ app_name = 'article'
 
 urlpatterns = [
     path('<int:pk>/', article.ArticleView.as_view(), name='article'),
-    path('create-article/', article.ArticleCreateView.as_view(), name='article_create'),
-    path('edit/<int:pk>/', article.ArticleEditView.as_view(), name='article_edit'),
+    path('create/', article.ArticleCreateView.as_view(), name='create'),
+    path('edit/<int:pk>/', article.ArticleEditView.as_view(), name='edit'),
 ]
