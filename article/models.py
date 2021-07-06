7cdf167f94562a7a6c47d782c6b3dc906b55d2f8
Igor <gormal98@gmail.com>
2021-07-06 12:45:44 +0300

    property_model_like

--- article/models.py
+++ article/models.py
@@ -57,12 +57,12 @@ class Like(models.Model):
         """Меняет значение is_liked на противоположное"""
         self.is_liked = not self.is_liked
         self.save()
-    
+
     def set_like(self):
         """Срабатывает при нажатии на кнопку 'like'"""
         self.is_liked = True
         self.is_disliked = False
-    
+
     def set_dislike(self):
         """Срабатывает при нажатии на кнопку 'dislike'"""
         self.is_liked = False
@@ -71,6 +71,14 @@ class Like(models.Model):
     def cancel_liked(self):
         self.is_liked = False
         self.is_disliked = False
+
+    @property
+    def is_like_set(self):
+        return self.is_liked and not self.is_disliked
+
+    @property
+    def is_dislike_set(self):
+        return not self.is_liked and self.is_disliked
 
 
 class Comment(models.Model):
