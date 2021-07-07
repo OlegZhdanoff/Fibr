bd1568e4f94d05c65edf2b591c03f05ebdc1c1bb
Kirill Ivanov <kirusha1991@gmail.com>
2021-07-07 15:04:51 +0500

    Comments Article template refactoring for comments Comment model methods for template rendering Article app url for comment Add comments to article view's context

--- article/urls.py
+++ article/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('edit/<int:pk>/', article.ArticleEditView.as_view(), name='edit'),
 
     path('like/<int:pk>', article.article_like, name='like_article'),
+    path('comment/<int:pk>', article.article_comment, name='comment_article'),
 
 ]
