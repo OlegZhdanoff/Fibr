7c1f7afdf5a402f45f7c79463b8060bec1bd54e0
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-07-08 00:39:47 +0300

    unnecessary

--- authapp/models.py
+++ authapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from article.models import *
 
 
 class User(AbstractUser):
