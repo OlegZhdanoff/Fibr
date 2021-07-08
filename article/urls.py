807b17a330072915c2f89805e8c16536b967c18b
OlegZhdanov <stalker08@gmail.com>
2021-07-08 21:18:43 +0500

    comment_article fixed

--- article/urls.py
+++ article/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:pk>/', article.ArticleView.as_view(), name='article'),
     path('create/', article.ArticleCreateView.as_view(), name='create'),
     path('edit/<int:pk>/', article.ArticleEditView.as_view(), name='edit'),
+    path('comment_article/<int:pk>/', article.article_comment, name='comment_article'),
 
     path('like/<int:pk>', article.article_like, name='like_article'),
     path('toggle/<int:pk>', article.article_toggle, name='toggle'),
