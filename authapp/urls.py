6fa1a23f3f258c48c44bf6e0c94f8071f3900bff
Alex87Rt <63261295+Alex87Rt@users.noreply.github.com>
2021-07-14 00:24:30 +0300

    get_profile_user

--- authapp/urls.py
+++ authapp/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('logout/', authapp.UserLogout.as_view(), name='logout'),
     path('register/', authapp.RegisterUserView.as_view(), name='register'),
     path('profile/<int:pk>/', authapp.ProfileView.as_view(), name='profile'),
-    # path('edit/', authapp.edit, name='edit'),
+    path('profile/<username>/', authapp.ProfileView.as_view(), name='user_profile'),
 ]
