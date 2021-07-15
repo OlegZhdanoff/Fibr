880d66a73916e6610c10791e953a91731599c60d
OlegZhdanov <stalker08@gmail.com>
2021-07-15 16:21:40 +0500

    moderation count badge

--- context_processor.py
+++ context_processor.py
@@ -1,6 +1,12 @@
+from article.models import Article
 from hub.models import Topic
 
 
 # Добавляет в контекст меню категорий(хабов)
 def topic_list(request):
     return {"topic_list": Topic.objects.all()}
+
+
+def moderated_article_count(self):
+    return {'moderated_articles_count': Article.get_moderated_articles().count()}
+#     return 2
